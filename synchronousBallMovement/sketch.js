986ebee4dd9afcc2dcfd51a5dd1ad3fbd601d606
saathvik-354 <59614071+saathvik-354@users.noreply.github.com>
2020-03-18 20:17:51 +0530

    Add files via upload

---
+++ synchronousBallMovement/sketch.js
@@ -0,0 +1,17 @@
+var canvas,backgroundImage,playerCount,form,game,player,database;
+var gameState = 0;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  
+}
+
